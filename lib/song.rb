919fa8fb39506a1f235ece327d2a881abbad5d85
Javier Aguilar <coyote176@msn.com>
2020-08-15 20:06:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   @@artists = []
   
  
-  def initialize(name, artists, genres)
+  def initialize(name, artist, genre)
     @@count += 1 
     @@artists << artists
     @@genres <<  genres
