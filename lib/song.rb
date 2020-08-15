9f6b60d7e42b848ae09333137f8b428d9a2c7cee
Javier Aguilar <coyote176@msn.com>
2020-08-15 19:43:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,20 @@ class Song
  attr_accessor :name, :artists, :genres 
   
   @@count = 0 
+  @@genres = []
+  @@artists = 
  
-  def initialize
+  def initialize(name)
     @@count += 1 
+    @name= name 
   end
   
   def self.count
     @@count
+  end
+  
+  def genres[]
+    @@[]
   end
   
 end
